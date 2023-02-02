fb5489ae353da9bd5de2ca13d73dadc3c4b403c6
Val_M <v.mischenko90@gmail.com>
2023-02-02 17:37:51 +0200

    add base dependencies

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+const App: FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>Working</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
